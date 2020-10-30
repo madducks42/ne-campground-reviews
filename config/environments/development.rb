806abed2f2b9f00557de011ee56e05fee7531238
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2020-10-30 18:13:31 -0400

    - Setup devise login and account creation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,15 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
+
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   :user_name => '3e5acc73bdeaed',
+#   :password => '37e846670294fb',
+#   :address => 'smtp.mailtrap.io',
+#   :domain => 'smtp.mailtrap.io',
+#   :port => '2525',
+#   :authentication => :cram_md5
+# }
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,6 +2,9 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
+    sequence(:username) {|n| "username#{n}"}
+    sequence(:first_name) {|n| "firstname#{n}"}
+    sequence(:last_name) {|n| "lastname#{n}"}
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
