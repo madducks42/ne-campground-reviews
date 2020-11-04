5afd2e04edf9542a7f029bec4a5a03ab59f55dfe
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2020-11-04 11:48:19 -0500

    WIP: JS not being found

---
+++ app/javascript/react/components/CampgroundShowPhotoTile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CampgroundShowPhotoTile = () => {
+  
+  return (
+    <div>
+      <a href='/images/test_image.jpg' data-lightbox='Test' data-title='Test'>
+        <img src='/images/test_image.jpg'/>
+      </a>
+    </div>
+  )
+}
+
+export default CampgroundShowPhotoTile
