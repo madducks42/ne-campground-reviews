fc55b5c2e1cbf7fc0baac7577cc7e5689ad3b8d9
Maddox Grey <maddoxgrey@Mag-Mell.fios-router.home>
2020-12-01 15:19:44 -0500

    WIP: still can't get multiple sections to display

--- app/javascript/react/components/MenuComponents/CamperModifications/CamperModsContainer.js
+++ app/javascript/react/components/MenuComponents/CamperModifications/CamperModsContainer.js
@@ -1,34 +1,75 @@
 import React, { useState, useEffect } from "react";
-
 import MattressUpgrade from "./CamperMods/MattressUpgrade";
-
 const CamperModsContainer = () => {
-  const [iconType, setIconType] = useState("");
-  const [show, setShow] = useState(false);
-  const [hide, setHide] = useState(true);
+  const [selectedSections, setSelectedSections] = useState([]);
+  const sectionsData = [
+    {
+      title: "Mattress Upgrade",
+      id: "mattressUpgrade",
+      body: (
+        <div>
+          <p>Mattress Upgrade Section</p>
+        </div>
+      ),
+    },
+    {
+      title: "Speaker Upgrade",
+      id: "speakerUpgrade",
+      body: (
+        <div>
+          <p>Mattress Upgrade Section</p>
+        </div>
+      ),
+    },
+  ];
 
-  useEffect(() => {
-    setIconType("fas fa-plus-square")
-    setShow(false)
-    setHide(true)
-  }, [])
+  // useEffect(() => {
+  //   setSelectedSections([])
+  // }, [//something needs to go here]);
 
-  let onClickHandler = () => {
-    toggleShow()
-  }
-
-  const toggleShow = () => {
-    if (hide === true) {
-      setIconType("fas fa-minus-square")
-      setShow(true)
-      setHide(false)
-    } else if (hide === false) {
-      setIconType("fas fa-plus-square")
-      setShow(false)
-      setHide(true)
+  const onClickHandler = (event) => {
+    event.preventDefault();
+    let newSections;
+    const id = event.currentTarget.id;
+    if (selectedSections.includes(id)) {
+      newSections = selectedSections.splice(selectedSections.indexOf(id, 1));
+      setSelectedSections(newSections);
+    } else {
+      newSections = selectedSections.push(id);
+      setSelectedSections(newSections);
     }
-  }
-
+    console.log(selectedSections);
+  };
+  const sections = sectionsData.map((section) => {
+    const minusIcon = "fas fa-minus-square";
+    const plusIcon = "fas fa-plus-square";
+    if (selectedSections.includes(section.id)) {
+      return (
+        <div className="flex-row">
+          <h3>{section.title}</h3>
+          <button
+            type="button"
+            id={section.id}
+            className={minusIcon}
+            onClick={onClickHandler}
+          ></button>
+          {section.body}
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex-row">
+          <h3>{section.title}</h3>
+          <button
+            type="button"
+            id={section.id}
+            className={plusIcon}
+            onClick={onClickHandler}
+          ></button>
+        </div>
+      );
+    }
+  });
   return (
     <div>
       <h1>Camper Modifications</h1>
@@ -36,18 +77,8 @@ const CamperModsContainer = () => {
         As much as we love the Wolf Pup (aka the &apos;Gray Ghost&apos;), there
         are some things that we tweaked...
       </p>
-      <div className="flex-row">
-        <h3>Mattress Upgrade</h3>
-        <button
-          type="button"
-          id= "mattress-upgrade"
-          className={iconType}
-          onClick={onClickHandler}
-        ></button>
-      </div>
-      {show === true && <MattressUpgrade />}
+      {sections}
     </div>
   );
 };
-
 export default CamperModsContainer;
