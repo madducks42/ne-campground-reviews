7cf00fe4119109731f67d4e9f4f1ad76dbcb640b
Maddox Grey <maddoxgrey@Mag-Mell.fios-router.home>
2020-12-03 14:34:36 -0500

    - Minor code cleanup

--- app/javascript/react/components/CampgroundShow/ShowComponents/MapTile.js
+++ app/javascript/react/components/CampgroundShow/ShowComponents/MapTile.js
@@ -5,7 +5,7 @@ const MapTile = () => {
   return (
     <div className="lightbox-container">
       <a href="/images/map.jpg" data-lightbox="map" data-title="Test Image">
-        <img className ="map-image" src="/images/map.jpg" />
+        <img className ="map-image" src="/images/map.jpg" alt="campground map"/>
       </a>
     </div>
   )
