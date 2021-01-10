c315b308be7732190d2305c1b9b0eb28f21b2b99
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2021-01-09 22:57:42 -0500

    Revert "Testing out mailgun for production"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :port => ENV['MAILGUN_SMTP_PORT'],
-    :address => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'necampgroundreviews.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
     :authentication => :plain,
+    :enable_starttls_auto => true
    }
 
   # Ignore bad email addresses and do not raise email delivery errors.
