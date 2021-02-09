700a0991f1b6ed474cced4ae3590620fb7f6a123
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-08 20:52:57 -0500

    Updated URL for production environment

--- app/javascript/react/components/AdminFeatures/ManageImages/ImageTiles.js
+++ app/javascript/react/components/AdminFeatures/ManageImages/ImageTiles.js
@@ -6,7 +6,10 @@ const ImageTiles = (props) => {
   
   return (
     <div className="m-sm">
-      <img className="image-sm" src={`https://campground-reviews-dev.s3.amazonaws.com/${url}`} alt="Campground"/>
+      {/* Dev env link */}
+      {/* <img className="image-sm" src={`https://campground-reviews-dev.s3.amazonaws.com/${url}`} alt="Campground" /> */}
+      {/* Prod env link */}
+      <img className="image-sm" src={`https://campground-reviews-production.s3.amazonaws.com/${url}`} alt="Campground"/>
       <h6 className="is-size-6 has-text-centered">Image Name: {name}</h6>
     </div>
   )
