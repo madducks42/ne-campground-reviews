39f3d283eeaa00444f45d2fbfdac922783a7cb96
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-10 23:31:04 -0500

    - Working on VCR config

--- spec/api/v1/campgrounds_controller_spec.rb
+++ spec/api/v1/campgrounds_controller_spec.rb
@@ -46,39 +46,33 @@ RSpec.describe Api::V1::CampgroundsController, type: :controller do
       expect(returned_response[1]['location']).to eq campground_2.location
     end
   end
-  
-  # describe 'OpenWeatherApiClient' do
-  #   describe 'weather_api' do
-  #     it "returns correctly some data", :vcr do
-  #       get :show, params: {id: campground_1.id}
-  #     returned_response = JSON.parse(response.body)
-  #     end
-  #   end
-  # end
 
   describe 'GET #show' do
-    it 'should return the name, description, location, amenities, and reviews of an individual campground on its own show page', VCR.insert_cassette("open_weather") do
-      get :show, params: {id: campground_1.id}
-      returned_response = JSON.parse(response.body)
-    
-      expect(response.status).to eq 200
-      expect(response.content_type).to eq 'application/json; charset=utf-8'
+    it 'should return the name, description, location, amenities, and reviews of an individual campground on its own show page' do 
+      VCR.use_cassette('open_weather') do
+        get :show, params: {id: campground_1.id}
+        
+        returned_response = JSON.parse(response.body)
+        
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq 'application/json; charset=utf-8'
 
-      expect(returned_response['name']).to eq campground_1.name
-      expect(returned_response['description']).to eq campground_1.description
-      expect(returned_response['location']).to eq campground_1.location
-      expect(returned_response['zip_code']).to eq campground_1.zip_code
-      expect(returned_response['dogs_allowed']).to eq campground_1.dogs_allowed
-      expect(returned_response['electric_hookups']).to eq campground_1.electric_hookups
-      expect(returned_response['water_hookups']).to eq campground_1.water_hookups
-      expect(returned_response['potable_water']).to eq campground_1.potable_water
-      expect(returned_response['dump_station']).to eq campground_1.dump_station
-      expect(returned_response['bathrooms']).to eq campground_1.bathrooms
-      expect(returned_response['showers']).to eq campground_1.showers
+        expect(returned_response['name']).to eq campground_1.name
+        expect(returned_response['description']).to eq campground_1.description
+        expect(returned_response['location']).to eq campground_1.location
+        expect(returned_response['zip_code']).to eq campground_1.zip_code
+        expect(returned_response['dogs_allowed']).to eq campground_1.dogs_allowed
+        expect(returned_response['electric_hookups']).to eq campground_1.electric_hookups
+        expect(returned_response['water_hookups']).to eq campground_1.water_hookups
+        expect(returned_response['potable_water']).to eq campground_1.potable_water
+        expect(returned_response['dump_station']).to eq campground_1.dump_station
+        expect(returned_response['bathrooms']).to eq campground_1.bathrooms
+        expect(returned_response['showers']).to eq campground_1.showers
 
-      expect(returned_response['reviews'][0]['title']).to eq campground_1.reviews[0]['title']
-      expect(returned_response['reviews'][0]['body']).to eq campground_1.reviews[0]['body']
-      expect(returned_response['reviews'][0]['rating']).to eq campground_1.reviews[0]['rating']
+        expect(returned_response['reviews'][0]['title']).to eq campground_1.reviews[0]['title']
+        expect(returned_response['reviews'][0]['body']).to eq campground_1.reviews[0]['body']
+        expect(returned_response['reviews'][0]['rating']).to eq campground_1.reviews[0]['rating']
+      end
     end
   end
 
