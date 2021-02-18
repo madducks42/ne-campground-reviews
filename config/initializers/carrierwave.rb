a3ee8f2ec623c4bd2bc1ccd5b080154f4d39019d
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2021-02-18 17:46:47 -0500

    WIP: Added default region for AWS S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'us-east-1'
     }
     if Rails.env.production?
       config.fog_directory  = ENV["S3_BUCKET_PROD"]
