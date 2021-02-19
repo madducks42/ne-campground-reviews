0593a5c53801eaa22a29f9241a5917c7ad464f28
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2021-02-19 18:23:38 -0500

    Amateur Hour: forgot to remove binding.pry

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ImagesController < ApiController
   skip_before_action :verify_authenticity_token, only: [:create, :update]
 
   def index
-    # binding.pry
     campground = Campground.find(params[:campground_id])
     images = campground.images
 
@@ -10,7 +9,6 @@ class Api::V1::ImagesController < ApiController
   end
 
   def create
-    binding.pry
     new_image = Image.new(image_params)
     
     if new_image.save
